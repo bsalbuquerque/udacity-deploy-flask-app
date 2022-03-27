f3e6003a30e0b5da55309d63997b1db6b0ca85b6
Bruno Albuquerque <contato@brunoalbuquerque.com>
2022-03-27 06:53:11 -0300

    Updating tests commit

--- test_main.py
+++ test_main.py
@@ -15,7 +15,7 @@ PASSWORD = 'huff-puff'
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = False
+    main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
     yield client
@@ -24,7 +24,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json != 'Healthy'
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
@@ -36,4 +36,4 @@ def test_auth(client):
 
     assert response.status_code == 200
     token = response.json['token']
-    assert token is not True
+    assert token is not None
